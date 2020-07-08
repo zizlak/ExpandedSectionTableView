906abf691812bb5c6fb72df4b897d8ccd0b885ec
Aleksandr Kurdiukov <runoleonard@gmail.com>
2020-07-08 17:49:08 +0200

    Initial Commit

---
+++ ExpandenCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpandenCell
+//
+//  Created by Aleksandr Kurdiukov on 08.07.20.
+//  Copyright © 2020 Zizlak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
